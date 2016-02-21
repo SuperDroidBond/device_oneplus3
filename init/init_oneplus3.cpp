ba45fe4359f354609f5732d5e780f1116f7416d1
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    Allow vendor init to override props

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -31,11 +31,25 @@
 #include <fcntl.h>
 #include <string>
 #include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "util.h"
 #include "log.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
 
 static int read_file2(const char *fname, char *data, int max_size)
 {
