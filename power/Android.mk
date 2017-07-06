3c79c40e6970a2c3f195c502d3796027d975dfce
maxwen <max.weninger@gmail.com>
2017-07-06 14:56:06 +0200

    op3(t): use marlin power hal
    seems to be hip these days
    
    Change-Id: I52672babaf0dee4e05723cd3e86a2a82c11d62bb

--- power/Android.mk
+++ power/Android.mk
@@ -6,10 +6,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c \
-                   util.c
-
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 LOCAL_MODULE := power.msm8996
-LOCAL_MODULE_TAGS := optional
 
+# Include target-specific files.
+LOCAL_SRC_FILES += power-8996.c
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+#ifneq ($(TARGET_USES_AOSP),true)
+#    LOCAL_CFLAGS += -DEXTRA_POWERHAL_HINTS
+#endif
+
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
