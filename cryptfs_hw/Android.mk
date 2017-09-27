272c4769db185f792bacc8380f805f68f463c431
maxwen <max.weninger@gmail.com>
2017-09-27 12:19:13 +0200

    op3(t): update cryptfs_hw
    Change-Id: Ib51f2bd29f293b5358ff546d8fc92e35318effdf

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -2,8 +2,6 @@ ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PROPRIETARY_MODULE := true
-
 sourceFiles := \
                cryptfs_hw.c
 
@@ -11,9 +9,14 @@ commonSharedLibraries := \
                         libcutils \
                         libutils \
                         libdl \
-                        libhardware
+                        libhardware \
+                        liblog
+
 commonIncludes := \
-                  hardware/libhardware/include/hardware/
+                  hardware/libhardware/include/hardware/ \
+                  $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
@@ -23,10 +26,6 @@ LOCAL_MODULE:= libcryptfs_hw
 LOCAL_SHARED_LIBRARIES := $(commonSharedLibraries)
 
 LOCAL_MODULE_OWNER := qcom
-
-ifeq ($(TARGET_SWV8_DISK_ENCRYPTION),true)
-LOCAL_CFLAGS += -DCONFIG_SWV8_DISK_ENCRYPTION
-endif
 
 # USE_ICE_FOR_STORAGE_ENCRYPTION would be true in future if
 # TARGET_USE_EMMC_USE_ICE is set
