a651545904dd21dbeffc739b555621bcf7e67c33
maxwen <max.weninger@gmail.com>
2017-09-30 17:36:41 +0200

    op3(t): add thermal and vr hidl service
    Change-Id: I30abd53cfae1101157b92bf9b869e6469915c9f5

--- hidl.mk
+++ hidl.mk
@@ -75,8 +75,10 @@ PRODUCT_PACKAGES += \
 
 #VR
 PRODUCT_PACKAGES += \
-    android.hardware.vr@1.0-impl
+    android.hardware.vr@1.0-impl \
+    android.hardware.vr@1.0-service
 
 #Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
