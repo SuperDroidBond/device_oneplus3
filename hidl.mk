c0d6eb6d0ef62b0f31d990a72f018bf11e86e488
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    op3: Switch to Soundtrigger 2.1 implementation implementation
    Bug: 68823037
    Test: "ok google" from locked screen

--- hidl.mk
+++ hidl.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
