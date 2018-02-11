ecdf96afbf728e5f1422dfa2689ace512b70ceae
SuperDroid-BOND <superdroidbond@yahoo.com>
2018-02-11 18:49:29 +0530

    Added OnePlus Shit (Device Features)

---
+++ OneplusShit/src/com/oneplus/shit/settings/ShitPanelSettings.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oneplus.shit.settings;
+
+import com.oneplus.shit.util.NodePreferenceActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.preference.TwoStatePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.util.Log;
+import com.oneplus.shit.R;
+
+public class ShitPanelSettings extends NodePreferenceActivity {
+    public static final String KEY_VIBSTRENGTH = "vib_strength";
+
+    public static final String KEY_TAPTOWAKE_SWITCH = "taptowake";
+
+    public static final String KEY_SRGB_SWITCH = "srgb";
+    public static final String KEY_HBM_SWITCH = "hbm";
+    public static final String KEY_DCI_SWITCH = "dci";
+
+    private VibratorStrengthPreference mVibratorStrength;
+    private TwoStatePreference mTapToWakeSwitch;
+    private TwoStatePreference mSRGBModeSwitch;
+    private TwoStatePreference mHBMModeSwitch;
+    private TwoStatePreference mDCIModeSwitch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.shit_panel);
+
+        mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
+        if (mVibratorStrength != null) {
+            mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
+        }
+
+        mTapToWakeSwitch = (TwoStatePreference) findPreference(KEY_TAPTOWAKE_SWITCH);
+        mTapToWakeSwitch.setEnabled(TapToWakeSwitch.isSupported());
+        mTapToWakeSwitch.setChecked(TapToWakeSwitch.isCurrentlyEnabled(this));
+        mTapToWakeSwitch.setOnPreferenceChangeListener(new TapToWakeSwitch());
+
+        mSRGBModeSwitch = (TwoStatePreference) findPreference(KEY_SRGB_SWITCH);
+        mSRGBModeSwitch.setEnabled(SRGBModeSwitch.isSupported());
+        mSRGBModeSwitch.setChecked(SRGBModeSwitch.isCurrentlyEnabled(this));
+        mSRGBModeSwitch.setOnPreferenceChangeListener(new SRGBModeSwitch());
+
+        mHBMModeSwitch = (TwoStatePreference) findPreference(KEY_HBM_SWITCH);
+        mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
+        mHBMModeSwitch.setChecked(HBMModeSwitch.isCurrentlyEnabled(this));
+        mHBMModeSwitch.setOnPreferenceChangeListener(new HBMModeSwitch());
+
+        mDCIModeSwitch = (TwoStatePreference) findPreference(KEY_DCI_SWITCH);
+        boolean isPanelSupported = DCIModeSwitch.isSupportedPanel();
+        if (isPanelSupported) {
+            mDCIModeSwitch.setEnabled(DCIModeSwitch.isSupported());
+            mDCIModeSwitch.setChecked(DCIModeSwitch.isCurrentlyEnabled(this));
+            mDCIModeSwitch.setOnPreferenceChangeListener(new DCIModeSwitch());
+        }
+    }
+}
---
+++ OneplusShit/src/com/oneplus/shit/util/Constants.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oneplus.shit.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class Constants {
+
+    public static final String BUTTON_SWAP_KEY = "button_swap";
+
+    // Button nodes
+    public static final String BUTTON_SWAP_NODE = "/proc/s1302/key_rep";
+
+    // Holds <preference_key> -> <proc_node> mapping
+    public static final Map<String, String> sBooleanNodePreferenceMap = new HashMap<>();
+    public static final Map<String, String> sStringNodePreferenceMap = new HashMap<>();
+
+    // Holds <preference_key> -> <default_values> mapping
+    public static final Map<String, Object> sNodeDefaultMap = new HashMap<>();
+
+    public static final String[] sButtonPrefKeys = {
+        BUTTON_SWAP_KEY,
+    };
+
+    static {
+        sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, BUTTON_SWAP_NODE);
+
+        sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
+    }
+
+    public static boolean isPreferenceEnabled(Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(key, (Boolean) sNodeDefaultMap.get(key));
+    }
+
+    public static String getPreferenceString(Context context, String key) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(key, (String) sNodeDefaultMap.get(key));
+    }
+}
