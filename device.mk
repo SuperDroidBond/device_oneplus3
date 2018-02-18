b8bd0ff545edd8cda9113f3500739be95172dd5b
SuperDroidBond <superdroidbond@yahoo.com>
2018-02-18 05:47:34 +0000

    OneplusShit: Introduce Spectrum Kernel Manager and access on boot

--- device.mk
+++ device.mk
@@ -248,6 +248,11 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
+# Spectrum
+PRODUCT_PACKAGES += \
+   init.spectrum.rc \
+   init.spectrum.sh
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
