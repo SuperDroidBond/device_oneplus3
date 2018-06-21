c52352663278d8df24d1ae3489d2e4df2844d3a6
SuperDroidBond <superdroidbond@yahoo.com>
2018-06-21 11:39:32 +0530

    Remove doze. We have our custom doze for all devices

--- device.mk
+++ device.mk
@@ -240,10 +240,6 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusThreeDoze
-
 # Device Parts
 PRODUCT_PACKAGES += \
     OneplusShit    
