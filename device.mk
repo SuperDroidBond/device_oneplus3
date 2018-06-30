8ecc83d2344c8fd25f86452706879d2a229361a8
SuperDroidBond <superdroidbond@yahoo.com>
2018-06-30 12:17:28 +0530

    Add DTS Support

--- device.mk
+++ device.mk
@@ -81,6 +81,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+PRODUCT_GENERIC_PROPERTIES += \
+    use.dts_eagle=true
+
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
