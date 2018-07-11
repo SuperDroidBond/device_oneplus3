6a78cec96e1c247e6a03ac736402709eef3bf91b
danny <noreference@web.de>
2018-07-11 09:06:14 +0200

    op3/t: fix the sepolicy [1/3]
    this commit contains the following changes:
    * sync to lineageos sources (recovery.fstab, init.target.rc, ...)
    * disable kernel cmd permissive line
    * fix some gzosp leftovers in scripts
    * add permissions for dash
    * add permissions for grahpics_composer
    * add permissions for mm-camerad
    * add permissions for vibrate control

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=oneplus
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+XTENDED_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+HELPER="$XTENDED_ROOT"/vendor/xtended/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -50,7 +50,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$XTENDED_ROOT"
 
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
 extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC"
@@ -58,7 +58,7 @@ extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh
 
-CAMERA_HAL="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8996.so
+CAMERA_HAL="$XTENDED_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8996.so
 
 sed -i \
     -e 's/_ZN7qcamera17QCameraParameters16setQuadraCfaModeEjb/_ZN7qcamera17QCameraParameters16setQuadraCfaModSHIM/' \
