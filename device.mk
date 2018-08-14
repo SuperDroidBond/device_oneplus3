2277aece9b4d8f620e398ffc06e13ecdb6744148
SuperDroidBond <superdroidbond@yahoo.com>
2018-08-14 22:28:31 +0530

    We don't the recovery updated. It's deprecated since nougat

--- device.mk
+++ device.mk
@@ -395,10 +395,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_op3
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
