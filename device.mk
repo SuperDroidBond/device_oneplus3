6c143e208e4073522ec6ddf70cbccbd266d0ce68
Haynes Mathew George <hgeorge@codeaurora.org>
2018-10-07 18:41:35 -0500

    system.prop: Set default period sizes
    Set new default period sizes for ull and low-latency-record
    audio usecases
    
    Bug: 29822375
    
    Change-Id: I75437818283f70dd285180f121bbb2e2b120e06c

--- device.mk
+++ device.mk
@@ -174,8 +174,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.hw.aac.encoder=true \
     ro.af.client_heap_size_kbyte=7168 \
     persist.vendor.audio.hw.binder.size_kbyte=1024 \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.media_vol_steps=30 \
+    ro.config.vc_call_vol_steps=15 \
+    audio_hal.period_multiplier=2
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
