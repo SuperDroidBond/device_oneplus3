7f116b1eceabf9751bba2b4cb6e47804ae64f5b3
andr68rus <ashtmb@gmail.com>
2018-12-10 22:13:32 +0530

    op3: add kernel profiles

--- device.mk
+++ device.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome
 
+# Spectrum (for initial config)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.spectrum.profile=0
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
@@ -403,6 +407,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.spectrum.rc \
     init.class_main.sh \
     init.qcom.power.rc \
     init.qcom.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -82,3 +82,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.sensors.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
