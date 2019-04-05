ab981ddcb1bc8e1c0647ad833b1710d9eaaed038
SuperDroidBond <superdroidbond@yahoo.com>
2019-04-05 23:21:07 +0530

    Increase Heap Size for hwui-memory

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@
 # Device was launched with M
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 $(call inherit-product, vendor/havoc/config/phone-xxhdpi-4096-dalvik-heap.mk)
