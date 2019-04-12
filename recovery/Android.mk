b9787bc55408fc072b64867211ee056311b33ebb
SuperDroidBond <superdroidbond@yahoo.com>
2019-04-12 22:46:10 +0530

    Get Back Recovery which removed
    This reverts commit 2277aece9b4d8f620e398ffc06e13ecdb6744148.

---
+++ recovery/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
+LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
+endif
+
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_op3
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)
