389f1df99619185b480352c660016fd24bf15939
SuperDroidBond <superdroidbond@yahoo.com>
2019-08-21 16:38:41 +0530

    ignore never allows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,5 +237,8 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+#ignore neverallows
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus3/BoardConfigVendor.mk
