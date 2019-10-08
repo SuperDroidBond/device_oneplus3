e7747e20d8984d4b7b24047897701e6815f016d2
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-09 00:22:58 +0530

    lower the display size

--- device.mk
+++ device.mk
@@ -247,7 +247,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES  += \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=420
+    ro.sf.lcd_density=408
 
 # Display calibration
 PRODUCT_PACKAGES += \
