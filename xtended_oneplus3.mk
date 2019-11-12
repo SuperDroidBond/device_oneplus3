c44670b8c5f10c5572dadebfa02d00b057658572
xyyx <xyyx@mail.ru>
2019-11-12 08:35:21 +0000

    Use 8.0 fingerprints to pass SafetyNet

--- xtended_oneplus3.mk
+++ xtended_oneplus3.mk
@@ -23,9 +23,6 @@ $(call inherit-product, device/oneplus/oneplus3/device.mk)
 # Inherit some common Xtended stuff.
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-# Vendor security patch level
-VENDOR_SECURITY_PATCH := 2019-04-01
-
 PRODUCT_NAME := xtended_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
@@ -34,6 +31,5 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 TARGET_VENDOR := oneplus
 
-BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR1.170623.032/02281230:user/release-keys"
 XTENDED_BUILD_TYPE := OFFICIAL
-
