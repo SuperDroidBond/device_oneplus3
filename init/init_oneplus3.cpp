40336d8eea6f4ce032ef9fb0f3d58a9c2b148475
SuperDroidBond <superdroidbond@yahoo.com>
2019-11-20 22:37:51 +0530

    Re-Add SaftyNet Hack after https://github.com/SuperDroidBond/device_oneplus3/commit/d1da73c034825299cfa4c69fac879418af4f6a3f

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -45,13 +45,13 @@ constexpr const char* RO_PROP_SOURCES[] = {
 };
 
 constexpr const char* BUILD_DESCRIPTION[] = {
-        "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys",
-        "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys",
+        "OnePlus3-user 8.0.0 OPR6.170623.013 77 release-keys",
+        "OnePlus3-user 8.0.0 OPR6.170623.013 77 release-keys",
 };
 
 constexpr const char* BUILD_FINGERPRINT[] = {
-        "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys",
-        "OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys",
+        "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR6.170623.013/10250816:user/release-keys",
+        "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR6.170623.013/10250816:user/release-keys",
 };
 
 void property_override(char const prop[], char const value[]) {
