1b4bee11429dd03e56854af94fed7bbe6851defc
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-27 23:55:28 +0530

    op3: use 6gb dalvik config

--- device.mk
+++ device.mk
@@ -26,8 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
