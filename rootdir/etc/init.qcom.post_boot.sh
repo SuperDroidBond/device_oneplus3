904071ba3a66af7b079845f7c56143dd94fd6cbd
mady51 <32254904+mady51@users.noreply.github.com>
2020-07-21 17:14:54 +0530

    update new thermals for msm8996

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4264,18 +4264,18 @@ case "$target" in
     setprop sys.io.scheduler "maple"
     # Thermal
     write /sys/kernel/msm_thermal/enabled 0
-    write /sys/kernel/msm_thermal/zone0 "1516800 2246400 41 40"
-    write /sys/kernel/msm_thermal/zone1 "1440000 2150400 42 41"
-    write /sys/kernel/msm_thermal/zone2 "1363200 2054400 43 42"
-    write /sys/kernel/msm_thermal/zone3 "1363200 1977600 44 43"
-    write /sys/kernel/msm_thermal/zone4 "1286400 1900800 45 44"
-    write /sys/kernel/msm_thermal/zone5 "1286400 1824000 47 45"
-    write /sys/kernel/msm_thermal/zone6 "1132800 1670400 49 47"
-    write /sys/kernel/msm_thermal/zone7 "1056000 1363200 54 49"
-    write /sys/kernel/msm_thermal/zone8 "902400 1056000 58 54"
-    write /sys/kernel/msm_thermal/zone9 "844800 902400 60 58"
-    write /sys/kernel/msm_thermal/zone10 "768000 748800 63 60"
-    write /sys/kernel/msm_thermal/sampling_ms 4000
+    write /sys/kernel/msm_thermal/zone0 "1516800 2150400 45 43"
+    write /sys/kernel/msm_thermal/zone1 "1440000 2150400 47 45"
+    write /sys/kernel/msm_thermal/zone2 "1363200 2054400 49 47"
+    write /sys/kernel/msm_thermal/zone3 "1363200 1977600 51 49"
+    write /sys/kernel/msm_thermal/zone4 "1286400 1900800 53 51"
+    write /sys/kernel/msm_thermal/zone5 "1286400 1824000 55 53"
+    write /sys/kernel/msm_thermal/zone6 "1132800 1670400 57 55"
+    write /sys/kernel/msm_thermal/zone7 "1056000 1363200 59 57"
+    write /sys/kernel/msm_thermal/zone8 "902400 1056000 61 59"
+    write /sys/kernel/msm_thermal/zone9 "844800 902400 63 61"
+    write /sys/kernel/msm_thermal/zone10 "768000 748800 65 63"
+    write /sys/kernel/msm_thermal/sampling_ms 6000
     write /sys/kernel/msm_thermal/enabled 1
 
     echo "14458,21687,43374,72290,101206,108435" > /sys/module/lowmemorykiller/parameters/minfree       
